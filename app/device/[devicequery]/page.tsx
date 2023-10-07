f1ab207ed873118dde478c7bcb54235e9fc8207a
victorgolda <vgolda86@gmail.com>
2023-10-07 14:27:01 -0600

    getData

--- app/device/[devicequery]/page.tsx
+++ app/device/[devicequery]/page.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
-export default function Device() {
-  return <h1>Hello, Home page!</h1>
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Device() {
+  const data = await getData()
+  console.log(data)
+  return <h1>{data.title}</h1>
 }

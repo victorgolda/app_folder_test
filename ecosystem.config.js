8dfa945656370ebb3397eb8de4212b2fe087ef99
victorgolda <vgolda86@gmail.com>
2023-10-07 11:52:12 -0600

    standalone

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'nitro-netvault',
-      script: './node_modules/next/dist/bin/next',
-      args: 'start -p ' + (process.env.PORT || 3000),
+      script: '.next/standalone/server.js',
+      args: '-p ' + (process.env.PORT || 3000),
       watch: false,
       autorestart: true
     }
